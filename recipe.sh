73db6d1a77eb6603b25820e206903edb8c76824e
Giuseppe Calabrese <git@giucal.it>
2020-04-22 18:08:43 +0200

    Add recipes execution script

---
+++ recipe.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+# Recipe executor.
+
+log() {
+    echo >&2 "$@"
+}
+
+usage() {
+    log "Usage: recipe [-h] <recipe> [<argument> ...]"
+}
+
+exec_recipe() {
+    recipe=$(find "$RECIPES_DIR" -name "$1" | head -n 1)
+
+    [ -z "$recipe" ] && {
+        log "Error: recipe not found: $1"
+        log "Available recipes:"
+        list_recipes
+        exit 2
+    }
+
+    shift
+    exec "$recipe" "$@"
+}
+
+list_recipes() {
+    find "$RECIPES_DIR" -type f -exec basename {} \;
+}
+
+
+[ -n "$RECIPES_DIR" ] || {
+    log "Error: variable RECIPES_DIR is not defined"
+    exit 1
+}
+[ -d "$RECIPES_DIR" ] || {
+    log "Error: recipes directory does not exist: $RECIPES_DIR"
+    exit 1
+}
+
+getopts h _ && usage && exit 2
+[ "$1" = -- ] && shift
+
+[ $# = 0 ] && list_recipes && exit
+exec_recipe "$@"
