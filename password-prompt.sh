cfd010d84fd7d9d622f1ab9e1f1ec57003039ddc
Giuseppe Calabrese <git@giucal.it>
2020-10-12 16:38:52 +0200

    Support password check
    Added a -c (check) option to ask for the password twice.

--- password-prompt.sh
+++ password-prompt.sh
@@ -3,32 +3,48 @@
 # This script prompts for a password using built-in shell capabilities.
 
 usage() {
-    echo >&2 "password-prompt: [-h] [-0] [-p <prompt>]"
+    echo >&2 "password-prompt: [-h] [-0c] [-p <prompt>]"
     [ "$1" = '-h' ] && echo >&2 "
 Options:
     -0  Terminate the password with a null character.
+    -c  Check password. (Ask twice.)
     -P <prompt>
         Prompt message. Default is 'Password: '."
     exit 2
 }
 
-prompt='Password: '
+message='Password: '
 null_terminated=
+check=
 
-while getopts "h0i:P:p:" opt; do
+while getopts "hc0i:P:p:" opt; do
     case $opt in
-        (P) prompt=$OPTARG      ;;
+        (c) check=t             ;;
+        (P) message=$OPTARG     ;;
         (0) null_terminated=t   ;;
         (h) usage -h            ;;
         (*) usage               ;;
     esac
 done
 
-printf '%s' "$prompt" > /dev/tty
+prompt() {
+    printf '%s' "$1" > /dev/tty
+    stty -echo
+    IFS= read -r "$2"
+    echo > /dev/tty
+    stty echo
+}
 
-stty -echo
-IFS= read -r pw
-stty echo
+while true; do
+    prompt "$message" password
+    [ -z "$check" ] && break
 
-printf '%s' "$pw"
+    # Check password.
+    prompt 'Retype: ' password_check
+    # shellcheck disable=SC2154
+    [ "$password" = "$password_check" ] && break ||
+        echo >&2 "$(basename "$0"): Passwords differ. Retry..."
+done
+
+printf '%s' "$password"
 [ -n "$null_terminated" ] && printf "\0"
